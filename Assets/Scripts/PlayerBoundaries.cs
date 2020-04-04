cfa7cbf5d62d972c1f48c40e2f766a50f50e9564
Linette Jaimes <linettejaimes@gmail.com>
2020-04-04 03:06:59 -0700

    Added boundaries limits to the character so it'll stay on screen

---
+++ Assets/Scripts/PlayerBoundaries.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBoundaries : MonoBehaviour
+{
+    private Vector2 scrBoundaries;  //Will hold the screen boundaries
+    public Camera MainCamera;   //Will hold the main camera of the scene 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Obtain the current screen boundaries
+        scrBoundaries = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+
+    }//End of Start
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 viewPosition = transform.position;
+        viewPosition.x = Mathf.Clamp(viewPosition.x, scrBoundaries.x * -1, scrBoundaries.x);
+        transform.position = viewPosition;
+        
+    }//End of LateUpdate
+
+
+}//End of PlayerBoundaries class
