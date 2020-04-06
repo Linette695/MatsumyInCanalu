870bdede401f0360265005503e55ab49aa32efb1
Linette Jaimes <linettejaimes@gmail.com>
2020-04-06 02:11:22 -0700

    Added a way to quit the game during gamemode

---
+++ Assets/Scripts/GameLevelController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLevelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if player has quitted the game using the escape key
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            #if UNITY_EDITOR
+             UnityEditor.EditorApplication.isPlaying = false;
+            #else
+              Application.Quit();
+            #endif
+        }
+
+    }//End of Update 
+
+
+}//End of GameLevelController class
