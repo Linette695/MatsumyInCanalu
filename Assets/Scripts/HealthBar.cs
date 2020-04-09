fe9170edc7de6abf0600b6dc02d1dadfdd0c9644
Linette Jaimes <linettejaimes@gmail.com>
2020-04-09 00:46:21 -0700

    Added health bar, still need to make it react to character collision

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthBar;
+    public PlayerHealth playerHealth;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerHealth = player.GetComponent<PlayerHealth>(); //Obtain the player health
+        healthBar = GetComponent<Slider>(); //Obtain the slider
+        healthBar.maxValue = playerHealth.maxHealth;    //Set the max value of healthBar 
+        healthBar.value = playerHealth.maxHealth;   //Set the default value of the healthBar
+
+    }//End of start
+
+    // Update is called once per frame
+    public void setHealth(int h) {
+        healthBar.value = h;
+    
+    }//End of setHealth
+}
---
+++ Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int currentH = 0; //Wil hold the current health of the player
+    public int maxHealth = 100; //The maximum health player can have
+    public HealthBar healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentH = maxHealth;   //Set the default health
+    }//End of Start
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            damageHealth(5);
+        }
+        
+    }//End of Update
+
+    public void damageHealth(int damage) {
+        if (currentH - damage < 0)
+        {
+            currentH = 0; 
+        }
+        else
+        {
+            currentH -= damage;  
+        }
+        healthBar.setHealth(currentH);  //Update the health bar
+    }//End of updateHEalth
+
+
+}//End of PlayerHealth class
+
